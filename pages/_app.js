69e0171ea5bf394df8e932e0ef7fce5e9b7fb6f1
mohsen <mbp.moh3n@gmail.com>
2022-06-02 11:25:32 +0430

    main Add progress bar for load pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import '../styles/globals.css'
-import { Provider } from 'react-redux'
-import { useStore } from '../redux/store/posts';
+import {Provider} from 'react-redux'
+import {useStore} from '../redux/store/posts';
+import NextNProgress from "nextjs-progressbar";
+function MyApp({Component, pageProps}) {
+    const store = useStore(pageProps.initialReduxState)
 
-function MyApp({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState)
-
-  return (
-      <Provider store={store}>
-        <Component {...pageProps} />
-      </Provider>
-  )
+    return (
+        <Provider store={store}>
+            <NextNProgress/>
+            <Component {...pageProps} />
+        </Provider>
+    )
 }
 
 export default MyApp
